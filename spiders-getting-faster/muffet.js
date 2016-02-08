876657743aebb82bf0088de21bf0761f61ee6085
walle303 <walle303@eastcoasthosting.cf>
2016-02-08 03:41:56 -0700

    Muffet + Alphabetical order

---
+++ spiders-getting-faster/muffet.js
@@ -0,0 +1,40 @@
+var muffet = new Howl({
+	src: ["audio/spiderdance.ogg"],
+	loop: true,
+});
+
+var update_time = new Date();
+
+var percent_time = 0.57686;
+
+var rate = 1;
+var rate_timer = percent_time;
+
+function update() {
+
+	var divwidth = document.getElementById('muffet_shaking').offsetWidth;
+	var divheight = document.getElementById('muffet_shaking').offsetHeight;
+
+	var new_time = new Date();
+	var delta = new_time.getTime() - update_time.getTime();
+	update_time.setTime(new_time.getTime());
+
+	rate_timer -= rate * delta / 1000;
+
+	if (rate_timer <= 0) {
+		rate_timer += percent_time;
+		rate += 0.01;
+		muffet.rate(rate);
+		document.getElementById("speed").innerHTML = "speed: " + (rate * 100).toFixed(0) + "%";
+	}
+
+	document.getElementById("muffet").style.top = ((rate - Math.random() * rate * 2)+((divheight-500)/2)) + "px";
+	document.getElementById("muffet").style.left = ((rate - Math.random() * rate * 2)+((divwidth-500)/2)) + "px";
+	requestAnimationFrame(update);
+}
+
+function run() {
+	muffet.play();
+	update_time = new Date();
+	requestAnimationFrame(update);
+}
