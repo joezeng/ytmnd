6bb44a4dfc7543dc3e88a6fa7add79031e454b0d
walle303 <walle303@eastcoasthosting.cf>
2016-02-08 01:40:00 -0700

    Dog Update

---
+++ errordog-getting-faster/annoydog.js
@@ -0,0 +1,40 @@
+var annoydog = new Howl({
+	src: ["audio/annoydog.ogg"],
+	loop: true,
+});
+
+var update_time = new Date();
+
+var percent_time = 0.57686;
+
+var rate = 1;
+var rate_timer = percent_time;
+
+function update() {
+
+	var divwidth = document.getElementById('annoying_dog').offsetWidth;
+	var divheight = document.getElementById('annoying_dog').offsetHeight;
+
+	var new_time = new Date();
+	var delta = new_time.getTime() - update_time.getTime();
+	update_time.setTime(new_time.getTime());
+
+	rate_timer -= rate * delta / 1000;
+
+	if (rate_timer <= 0) {
+		rate_timer += percent_time;
+		rate += 0.01;
+		annoydog.rate(rate);
+		document.getElementById("speed").innerHTML = "speed: " + (rate * 100).toFixed(0) + "%";
+	}
+
+	document.getElementById("annoydog").style.top = ((rate - Math.random() * rate * 2)+((divheight-500)/2)) + "px";
+	document.getElementById("annoydog").style.left = ((rate - Math.random() * rate * 2)+((divwidth-500)/2)) + "px";
+	requestAnimationFrame(update);
+}
+
+function run() {
+	annoydog.play();
+	update_time = new Date();
+	requestAnimationFrame(update);
+}
